18b692996aa9112b143d50137d1148be82ae60d6
panutat-p <pp.panutat@gmail.com>
2023-04-05 00:51:46 +0700

    feat: reverse string

---
+++ other/reverse_string.go
@@ -0,0 +1,15 @@
+package other
+
+func Reverse(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	if len(s) == 1 {
+		return s
+	}
+
+	lastIdx := len(s) - 1
+	lastChar := string(s[lastIdx])
+	return lastChar + Reverse(s[:lastIdx])
+}
---
+++ other/reverse_string_test.go
@@ -0,0 +1,19 @@
+package other
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	got := Reverse("hello")
+	want := "olleh"
+	if got != want {
+		t.Error("want:", want, "but got:", got)
+	}
+}
+
+func TestReverse2(t *testing.T) {
+	got := Reverse("Go is the best")
+	want := "tseb eht si oG"
+	if got != want {
+		t.Error("want:", want, "but got:", got)
+	}
+}
