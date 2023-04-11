3f10d753c9b361b6b2d3da9f61893f1de3c4fbed
panutat-p <pp.panutat@gmail.com>
2023-04-12 01:09:26 +0700

    test: generate permutation

--- array/permutation_test.go
+++ array/permutation_test.go
@@ -6,6 +6,19 @@ import (
 )
 
 func TestGeneratePermutation(t *testing.T) {
+	given := "12"
+	want := []string{
+		"12",
+		"21",
+	}
+	got := GeneratePermutation(given, 0, []string{})
+	if !reflect.DeepEqual(got, want) {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestGeneratePermutation2(t *testing.T) {
+	given := "123"
 	want := []string{
 		"123",
 		"132",
@@ -14,7 +27,7 @@ func TestGeneratePermutation(t *testing.T) {
 		"321",
 		"312",
 	}
-	got := GeneratePermutation("123", 0, []string{})
+	got := GeneratePermutation(given, 0, []string{})
 	if !reflect.DeepEqual(got, want) {
 		t.Error("want", want, "but got", got)
 	}
