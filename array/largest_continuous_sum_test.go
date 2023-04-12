0c031a07e8b5c3bc2c8c9fda74d6a26ca666ee7e
panutat-p <pp.panutat@gmail.com>
2023-04-13 01:35:44 +0700

    test: add more cases

--- array/largest_continuous_sum_test.go
+++ array/largest_continuous_sum_test.go
@@ -2,14 +2,23 @@ package array
 
 import "testing"
 
-func TestLargestSum(t *testing.T) {
-	r := LargestSum([]int{1, 2, -1, 3, 4, 10, 10, -10, -1})
-	if r != 29 {
-		t.Error("want 29 but got", r)
+func TestLargestSum_drop_left(t *testing.T) {
+	want := 15
+	got := LargestSum([]int{-50, 3, 6, 4, 2})
+	if want != got {
+		t.Error("want", want, "but got", got)
 	}
 }
 
-func TestLargestSum2(t *testing.T) {
+func TestLargestSum_drop_right(t *testing.T) {
+	want := 15
+	got := LargestSum([]int{3, 6, 4, 2, -50})
+	if want != got {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestLargestSum_cross(t *testing.T) {
 	want := 25
 	got := LargestSum([]int{1, 2, -1, 3, 10, 10, -10 - 1})
 	if want != got {
