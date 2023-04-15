55f74943e6ae700b83cb4c9cbe33bb96e5f19f31
panutat-p <pp.panutat@gmail.com>
2023-04-16 00:55:14 +0700

    chore: proof byte slice and rune slice

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(Reverse("hello"))
+	fmt.Println(Reverse("🐵 eat 🍌"))
+}
+
+func Reverse(str string) string {
+	var (
+		sb strings.Builder
+	)
+
+	rsl := []rune(str)
+	for i := len(rsl) - 1; i > -1; i -= 1 {
+		sb.WriteRune(rsl[i])
+	}
+
+	return sb.String()
+}
