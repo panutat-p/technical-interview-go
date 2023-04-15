5205a050d13cd0883e0deb4a33bdfcca6250231e
panutat-p <pp.panutat@gmail.com>
2023-04-16 01:38:57 +0700

    feat: palindrome

--- palindrome/reverse.go
+++ palindrome/reverse.go
@@ -1,1 +1,27 @@
 package palindrome
+
+import (
+	"unicode"
+)
+
+func Check1(str string) bool {
+	var (
+		original []rune
+	)
+	// filter only alphabet in lowercase
+	for _, r := range str {
+		if unicode.IsLetter(r) {
+			original = append(original, unicode.ToLower(r))
+		}
+	}
+
+	var (
+		reversed []rune
+	)
+	// reverse the string
+	for i := len(original) - 1; i > -1; i -= 1 {
+		reversed = append(reversed, original[i])
+	}
+
+	return string(original) == string(reversed)
+}
--- palindrome/reverse_test.go
+++ palindrome/reverse_test.go
@@ -1,6 +1,24 @@
 package palindrome
 
-func Check1(str string) bool {
+import "testing"
 
-	return false
+func TestCheck1_simple(t *testing.T) {
+	yes := Check1("Anna")
+	if !yes {
+		t.Error("want true")
+	}
+}
+
+func TestCheck1_comma(t *testing.T) {
+	yes := Check1("A man, a plan, a canal, Panama!")
+	if !yes {
+		t.Error("want true")
+	}
+}
+
+func TestCheck1_special_character(t *testing.T) {
+	yes := Check1("Was it a car or a cat I saw?")
+	if !yes {
+		t.Error("want true")
+	}
 }
