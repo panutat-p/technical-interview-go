3a82f4b2cbdfb7be55460ab01b3b5b6387ffff44
panutat-p <pp.panutat@gmail.com>
2023-04-17 00:16:26 +0700

    refactor: shortest word distance

--- shortest_word_distance/init_max.go
+++ shortest_word_distance/init_max.go
@@ -3,33 +3,31 @@ package shortest_word_distance
 func Shortest(words []string, word1, word2 string) int {
 	var (
 		a   = -1
-		b   = -1
-		ans = len(words) // the longest distance is len(words) - 1
+		b   = len(words)
+		ans = len(words) // the possible longest distance is len(words) - 1
 	)
 	for i, e := range words {
-		if e == word1 {
+		switch e {
+		case word1:
 			a = i
-		} else if e == word2 {
+			ans = min(ans, distance(a, b))
+		case word2:
 			b = i
-		}
-
-		if a != -1 && b != -1 {
-			temp := Diff(a, b)
-			ans = Min(ans, temp)
+			ans = min(ans, distance(a, b))
 		}
 	}
 
 	return ans
 }
 
-func Min(a, b int) int {
+func min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-func Diff(a, b int) int {
+func distance(a, b int) int {
 	if a > b {
 		return a - b
 	}
