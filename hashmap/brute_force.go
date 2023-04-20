1c02314b9c5386722b716c4ce8a76592054098e6
panutat-p <pp.panutat@gmail.com>
2023-04-20 23:22:54 +0700

    test: find missing element

---
+++ hashmap/brute_force.go
@@ -0,0 +1,28 @@
+package hashmap
+
+func Find2(arr1 []int, arr2 []int) int {
+	for _, v := range arr1 {
+		idx := index(arr2, v)
+		if idx == -1 {
+			// not found in arr2
+			return v
+		} else {
+			// found in arr2
+			arr2 = pop(arr2, idx)
+		}
+	}
+	return -1
+}
+
+func index(arr []int, num int) int {
+	for i, e := range arr {
+		if e == num {
+			return i
+		}
+	}
+	return -1
+}
+
+func pop(nums []int, idx int) []int {
+	return append(nums[:idx], nums[idx+1:]...)
+}
---
+++ hashmap/brute_force_test.go
@@ -0,0 +1,11 @@
+package hashmap
+
+import "testing"
+
+func TestFind2_missing_five(t *testing.T) {
+	want := 5
+	got := Find2([]int{5, 5, 5, 5, 7, 7}, []int{5, 5, 5, 7, 7})
+	if got != want {
+		t.Error("want", want, "but got", got)
+	}
+}
