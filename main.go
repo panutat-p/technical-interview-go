381849e1dd796947338ef49403040df21c80bcb3
panutat-p <pp.panutat@gmail.com>
2023-04-21 00:15:05 +0700

    chore: pair sum using two pointers

--- main.go
+++ main.go
@@ -3,20 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	PrintPermutation([]rune{'A', 'B', 'C'}, 0, 3)
+	FindPairSum([]int{6, 4, 2, 1, 3}, 6)
+	FindPairSum([]int{9, 11, 2, 5}, 11)
 }
 
-// PrintPermutation
-// https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string
-func PrintPermutation(input []rune, a, b int) {
-	if a == b {
-		fmt.Println(string(input))
-		return
+func FindPairSum(nums []int, target int) {
+	nums = sort(nums)
+
+	var (
+		i int
+		j = len(nums) - 1
+	)
+	for i < j {
+		if nums[i]+nums[j] < target {
+			i += 1
+		} else if nums[i]+nums[j] > target {
+			j -= 1
+		} else {
+			fmt.Printf("index (%v, %v) ", i, j)
+			fmt.Printf("value (%v, %v)\n", nums[i], nums[j])
+			return
+		}
+	}
+}
+
+func sort(nums []int) []int {
+	if len(nums) == 0 {
+		return []int{}
+	}
+	if len(nums) == 1 {
+		return nums
 	}
 
-	for i := a; i < b; i += 1 {
-		input[i], input[a] = input[a], input[i]
-		PrintPermutation(input, a+1, b)
-		input[i], input[a] = input[a], input[i]
+	pivot := nums[0]
+	var (
+		left  []int
+		right []int
+	)
+	for _, e := range nums[1:] {
+		if pivot > e {
+			left = append(left, e)
+		} else {
+			right = append(right, e)
+		}
 	}
+
+	nums = append(sort(left), pivot)
+	nums = append(nums, sort(right)...)
+	return nums
 }
