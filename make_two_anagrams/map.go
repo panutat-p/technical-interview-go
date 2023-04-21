ba9a6df92ab12046c56d940cca6207df838073ff
panutat-p <pp.panutat@gmail.com>
2023-04-22 01:49:19 +0700

    docs: make two anagrams using map

--- make_two_anagrams/map.go
+++ make_two_anagrams/map.go
@@ -1,5 +1,8 @@
 package make_two_anagrams
 
+// Make1
+// How many steps to make s2 to be an anagram with s1
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram
 func Make1(s1, s2 string) int {
 	var (
 		m = make(map[rune]int)
