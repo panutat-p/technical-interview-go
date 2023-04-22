7b44cacaa6b44b05991e1ee497f0ced213d2480f
panutat-p <pp.panutat@gmail.com>
2023-04-23 01:52:08 +0700

    feat: group anagrams, pairing logic

--- group_anagrams/string.go
+++ group_anagrams/string.go
@@ -1,9 +1,43 @@
 package group_anagrams
 
+import "fmt"
+
 // Group1
 // group the anagrams together
-// return: array of group of string in any order
+// return: alphabet of group of string in any order
 // https://leetcode.com/problems/group-anagrams
 func Group1(words []string) [][]string {
-	return [][]string{}
+	if len(words) == 0 {
+		return [][]string{}
+	}
+
+	if len(words) == 1 {
+		return [][]string{words}
+	}
+
+	var (
+		m   = make(map[[26]int]string)
+		ans [][]string
+	)
+	for _, e := range words {
+		alp := alphabet(e)
+		if _, ok := m[alp]; ok {
+			ans = append(ans, []string{m[alp], e})
+			delete(m, alp)
+		} else {
+			m[alp] = e
+		}
+	}
+	fmt.Println("m", m)
+	return ans
+}
+
+func alphabet(str string) [26]int {
+	var (
+		arr [26]int
+	)
+	for _, r := range str {
+		arr[r-'a'] += 1
+	}
+	return arr
 }
--- group_anagrams/string_test.go
+++ group_anagrams/string_test.go
@@ -21,6 +21,23 @@ func TestGroup1_simple(t *testing.T) {
 	}
 }
 
+func TestGroup1_3_chars(t *testing.T) {
+	want := [][]string{
+		{"bat"},
+		{"nat", "tan"},
+		{"ate", "eat", "tea"},
+	}
+	got := Group1([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+
+	for _, e1 := range got {
+		sort.Strings(e1)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Error("want", want, "but got", got)
+	}
+}
+
 func TestGroup1_one(t *testing.T) {
 	want := [][]string{
 		{"a"},
@@ -38,7 +55,7 @@ func TestGroup1_one(t *testing.T) {
 
 func TestGroup1_empty(t *testing.T) {
 	want := [][]string{
-		{},
+		{""},
 	}
 	got := Group1([]string{""})
 
