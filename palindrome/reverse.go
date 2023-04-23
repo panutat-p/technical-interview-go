bdc4dae7ff94a16b27295f4a8cba256b53b05ac7
panutat-p <pp.panutat@gmail.com>
2023-04-24 02:07:10 +0700

    fix: palindrome, alphanumeric characters

--- palindrome/reverse.go
+++ palindrome/reverse.go
@@ -4,13 +4,16 @@ import (
 	"unicode"
 )
 
+// Check1
+// Read only alphanumeric characters
+// https://leetcode.com/problems/valid-palindrome
 func Check1(str string) bool {
 	var (
 		original []rune
 	)
 	// filter only alphabet in lowercase
 	for _, r := range str {
-		if unicode.IsLetter(r) {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
 			original = append(original, unicode.ToLower(r))
 		}
 	}
@@ -18,7 +21,6 @@ func Check1(str string) bool {
 	var (
 		reversed []rune
 	)
-	// reverse the string
 	for i := len(original) - 1; i > -1; i -= 1 {
 		reversed = append(reversed, original[i])
 	}
--- palindrome/reverse_test.go
+++ palindrome/reverse_test.go
@@ -2,10 +2,17 @@ package palindrome
 
 import "testing"
 
-func TestCheck1_simple(t *testing.T) {
+func TestCheck1_true(t *testing.T) {
 	yes := Check1("Anna")
 	if !yes {
 		t.Error("want true")
+	}
+}
+
+func TestCheck1_false(t *testing.T) {
+	no := Check1("race a car")
+	if no {
+		t.Error("want false")
 	}
 }
 
@@ -18,6 +25,20 @@ func TestCheck1_comma(t *testing.T) {
 
 func TestCheck1_special_character(t *testing.T) {
 	yes := Check1("Was it a car or a cat I saw?")
+	if !yes {
+		t.Error("want true")
+	}
+}
+
+func TestCheck1_number(t *testing.T) {
+	no := Check1("0P")
+	if no {
+		t.Error("want false")
+	}
+}
+
+func TestCheck1_empty(t *testing.T) {
+	yes := Check1(" ")
 	if !yes {
 		t.Error("want true")
 	}
