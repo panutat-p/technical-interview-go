cd2ebabff96dc717fcc8a978631c8ebcf23634e0
panutat-p <pp.panutat@gmail.com>
2023-04-24 00:48:07 +0700

    test: rearrange

--- longest_palindrome/map_test.go
+++ longest_palindrome/map_test.go
@@ -2,14 +2,6 @@ package longest_palindrome
 
 import "testing"
 
-func TestLongest1_empty(t *testing.T) {
-	want := 0
-	got := Longest1("")
-	if want != got {
-		t.Error("want", want, "but got", got)
-	}
-}
-
 func TestLongest1_4_chars(t *testing.T) {
 	want := 4
 	got := Longest1("aabb")
@@ -21,6 +13,14 @@ func TestLongest1_4_chars(t *testing.T) {
 func TestLongest1_9_chars(t *testing.T) {
 	want := 7
 	got := Longest1("abccccdd")
+	if want != got {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestLongest1_empty(t *testing.T) {
+	want := 0
+	got := Longest1("")
 	if want != got {
 		t.Error("want", want, "but got", got)
 	}
