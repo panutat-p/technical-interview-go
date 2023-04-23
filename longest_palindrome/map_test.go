b2af8078713075998daa85ca84112f6303ac40cf
panutat-p <pp.panutat@gmail.com>
2023-04-24 01:09:16 +0700

    test: longest palindrome, uppercase

--- longest_palindrome/map_test.go
+++ longest_palindrome/map_test.go
@@ -2,9 +2,25 @@ package longest_palindrome
 
 import "testing"
 
-func TestLongest1_4_chars(t *testing.T) {
+func TestLongest1_simple(t *testing.T) {
 	want := 4
 	got := Longest1("aabb")
+	if want != got {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestLongest1_odd(t *testing.T) {
+	want := 7
+	got := Longest1("aaaaaaa")
+	if want != got {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestLongest1_one_pair_one_char(t *testing.T) {
+	want := 3
+	got := Longest1("aabcdef")
 	if want != got {
 		t.Error("want", want, "but got", got)
 	}
@@ -13,6 +29,14 @@ func TestLongest1_4_chars(t *testing.T) {
 func TestLongest1_9_chars(t *testing.T) {
 	want := 7
 	got := Longest1("abccccdd")
+	if want != got {
+		t.Error("want", want, "but got", got)
+	}
+}
+
+func TestLongest1_uppercase(t *testing.T) {
+	want := 7
+	got := Longest1("aaaAaaaa")
 	if want != got {
 		t.Error("want", want, "but got", got)
 	}
