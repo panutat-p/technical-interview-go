ed43bd2644acaee1f22b71d0afc739dde0df00f3
panutat-p <pp.panutat@gmail.com>
2023-04-23 21:36:05 +0700

    test: group anagrams, implement assertion

--- group_anagrams/string_test.go
+++ group_anagrams/string_test.go
@@ -6,19 +6,30 @@ import (
 	"testing"
 )
 
-func TestGroup1_simple(t *testing.T) {
-	want := [][]string{
-		{"dog", "god"},
+func assert(t *testing.T, got, want [][]string) {
+	if len(want) != len(got) {
+		t.Error("want", want, "but got", got)
+		return
 	}
-	got := Group1([]string{"god", "dog"})
 
-	for _, e1 := range got {
-		sort.Strings(e1)
+	sort.SliceStable(got, func(i, j int) bool {
+		return len(got[i]) < len(got[j])
+	})
+	for _, e := range got {
+		sort.Strings(e)
 	}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Error("want", want, "but got", got)
 	}
+}
+
+func TestGroup1_simple(t *testing.T) {
+	want := [][]string{
+		{"dog", "god"},
+	}
+	got := Group1([]string{"god", "dog"})
+	assert(t, got, want)
 }
 
 func TestGroup1_3_chars(t *testing.T) {
@@ -28,14 +39,7 @@ func TestGroup1_3_chars(t *testing.T) {
 		{"ate", "eat", "tea"},
 	}
 	got := Group1([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
-
-	for _, e1 := range got {
-		sort.Strings(e1)
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Error("want", want, "but got", got)
-	}
+	assert(t, got, want)
 }
 
 func TestGroup1_one(t *testing.T) {
@@ -43,14 +47,7 @@ func TestGroup1_one(t *testing.T) {
 		{"a"},
 	}
 	got := Group1([]string{"a"})
-
-	for _, e1 := range got {
-		sort.Strings(e1)
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Error("want", want, "but got", got)
-	}
+	assert(t, got, want)
 }
 
 func TestGroup1_empty(t *testing.T) {
@@ -58,12 +55,5 @@ func TestGroup1_empty(t *testing.T) {
 		{""},
 	}
 	got := Group1([]string{""})
-
-	for _, e1 := range got {
-		sort.Strings(e1)
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Error("want", want, "but got", got)
-	}
+	assert(t, got, want)
 }
