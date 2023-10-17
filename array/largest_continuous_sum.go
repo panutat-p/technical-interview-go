6c6cd4fbdb7ae911d91fb7860532c50d80e23457
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-10-18 00:21:35 +0700

    feat: max sub array

--- array/largest_continuous_sum.go
+++ array/largest_continuous_sum.go
@@ -2,6 +2,10 @@ package array
 
 import "github.com/panutat-p/technical-interview-go/pkg"
 
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
 func LargestSum(arr []int) int {
 	if len(arr) == 0 {
 		return 0
@@ -18,4 +22,21 @@ func LargestSum(arr []int) int {
 	}
 
 	return best
+}
+
+func Sum2(nums []int) int {
+    var sum int
+    var ans = nums[0]
+    for _, e := range nums {
+        sum = max(sum+e, e)
+        ans = max(ans, sum)
+    }
+    return ans
+}
+
+func max(a, b int) int {
+    if a >= b {
+        return a
+    }
+    return b
 }
