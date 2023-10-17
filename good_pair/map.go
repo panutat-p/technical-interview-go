6f4c790bf75724f2dfed048c19e6706d445ee57a
Panutat <49073281+panutat-p@users.noreply.github.com>
2023-10-18 00:07:45 +0700

    feat: good pairs

--- good_pair/map.go
+++ good_pair/map.go
@@ -2,8 +2,10 @@ package good_pair
 
 import "fmt"
 
-// Count1
+// 1512. Number of Good Pairs
+// https://leetcode.com/problems/number-of-good-pairs
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j
+
 func Count1(nums []int) int {
 	var (
 		m   = make(map[int]int)
@@ -17,4 +19,21 @@ func Count1(nums []int) int {
 
 	fmt.Println(m)
 	return ans
+}
+
+func Count2(nums []int) int {
+    var (
+        ans int
+        m = make(map[int]int)
+    )
+    for _, e := range nums {
+        if _, ok := m[e]; ok {
+            ans += m[e]
+            m[e] += 1
+        } else {
+            m[e] = 1
+        }
+    }
+    fmt.Println("m:", m)
+    return ans
 }
