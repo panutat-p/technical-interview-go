7e0ce248b067f4204550cb7d4078ccce6e17d178
panutat.p <pp.panutat@gmail.com>
2023-10-29 02:24:42 +0700

    test: remove digit, use t.Run

--- easy/remove_one_digit_from_string/significant_digit_test.go
+++ easy/remove_one_digit_from_string/significant_digit_test.go
@@ -2,34 +2,33 @@ package remove_one_digit_from_string
 
 import "testing"
 
-func TestSignificantDigit_back(t *testing.T) {
-	want := "12"
-	got := SignificantDigit("123", '3')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestSignificantDigit_front(t *testing.T) {
-	want := "231"
-	got := SignificantDigit("1231", '1')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestSignificantDigit_duplicated(t *testing.T) {
-	want := "51"
-	got := SignificantDigit("551", '5')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestSignificantDigit_middle(t *testing.T) {
-	want := "779547853679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878"
-	got := SignificantDigit("7795478535679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878", '5')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
+func TestSignificantDigit(t *testing.T) {
+	t.Run("remove at back", func(t *testing.T) {
+		want := "12"
+		got := SignificantDigit("123", '3')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("remove at front", func(t *testing.T) {
+		want := "231"
+		got := SignificantDigit("1231", '1')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("duplicated", func(t *testing.T) {
+		want := "51"
+		got := SignificantDigit("551", '5')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("remove at mid", func(t *testing.T) {
+		want := "779547853679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878"
+		got := SignificantDigit("7795478535679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878", '5')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
 }
--- easy/remove_one_digit_from_string/string_comparison_test.go
+++ easy/remove_one_digit_from_string/string_comparison_test.go
@@ -4,34 +4,33 @@ import (
 	"testing"
 )
 
-func TestStringComparison_back(t *testing.T) {
-	want := "12"
-	got := StringComparison("123", '3')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestStringComparison_front(t *testing.T) {
-	want := "231"
-	got := StringComparison("1231", '1')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestStringComparison_duplicated(t *testing.T) {
-	want := "51"
-	got := StringComparison("551", '5')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
-}
-
-func TestStringComparison_middle(t *testing.T) {
-	want := "779547853679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878"
-	got := StringComparison("7795478535679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878", '5')
-	if got != want {
-		t.Error("\nwant", want, "\ngot ", got)
-	}
+func TestStringComparison(t *testing.T) {
+	t.Run("remove at back", func(t *testing.T) {
+		want := "12"
+		got := StringComparison("123", '3')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("remove at front", func(t *testing.T) {
+		want := "231"
+		got := StringComparison("1231", '1')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("duplicated", func(t *testing.T) {
+		want := "51"
+		got := StringComparison("551", '5')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
+	t.Run("remove at mid", func(t *testing.T) {
+		want := "779547853679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878"
+		got := StringComparison("7795478535679443616467964135298543163376223791274561861738666981419251859535331546947347395531332878", '5')
+		if got != want {
+			t.Error("\nwant", want, "\ngot ", got)
+		}
+	})
 }
