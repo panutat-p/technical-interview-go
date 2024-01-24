1d58a2d8d9662cfb09a18c9fb31d0cc85fc6500f
panutat.p <pp.panutat@gmail.com>
2024-01-25 00:14:41 +0700

    refactor: rename, word pattern, two maps

--- easy/word_pattern/two_maps.go
+++ easy/word_pattern/two_maps.go
@@ -7,29 +7,29 @@ import (
 // TwoMaps
 // 290. Word Pattern
 // https://leetcode.com/problems/word-pattern
-func TwoMaps(pattern, input string) bool {
+func TwoMaps(patternString, input string) bool {
 	var (
-		m1    = make(map[rune]string)
-		m2    = make(map[string]rune)
-		sl    = []rune(pattern)
-		words = strings.Split(input, " ")
+		m1      = make(map[rune]string)
+		m2      = make(map[string]rune)
+		pattern = []rune(patternString)
+		words   = strings.Split(input, " ")
 	)
 
-	if len(sl) != len(words) {
+	if len(pattern) != len(words) {
 		return false
 	}
 
-	for i := 0; i < len(sl); i += 1 {
-		r := sl[i]
-		word := words[i]
-		m1[r] = word
-		m2[word] = r
+	for i := 0; i < len(pattern); i += 1 {
+		c := pattern[i]
+		w := words[i]
+		m1[c] = w
+		m2[w] = c
 	}
 
-	for i := 0; i < len(sl); i += 1 {
-		r := sl[i]
-		word := words[i]
-		if m1[r] != word || m2[word] != r {
+	for i := 0; i < len(pattern); i += 1 {
+		c := pattern[i]
+		w := words[i]
+		if m1[c] != w || m2[w] != c {
 			return false
 		}
 	}
